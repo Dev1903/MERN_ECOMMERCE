986f3a160a25984a39c29e1c5e55ea3415ffa8bc
Dev1903 <bristidev2004@gmail.com>
2024-09-17 22:36:32 +0530

    admin panel changes

--- client/src/admin-panel/components/Sidebar.jsx
+++ client/src/admin-panel/components/Sidebar.jsx
@@ -1,17 +1,24 @@
-// Sidebar.js
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import '../css/sidebar.css'; // Ensure you have a CSS file to include styling
 
 const Sidebar = () => {
     const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [sidebarOpen, setSidebarOpen] = useState(true); // State to manage sidebar visibility
 
     const toggleDropdown = () => {
         setDropdownOpen(!dropdownOpen);
     };
 
+    const toggleSidebar = () => {
+        setSidebarOpen(!sidebarOpen);
+    };
+
     return (
-        <div className="sidebar-admin">
+        <div className={`sidebar-admin ${sidebarOpen ? 'open' : 'closed'}`}>
+            <button className="sidebar-toggle" onClick={toggleSidebar}>
+                <i className={`fas ${sidebarOpen ? 'fa-chevron-left' : 'fa-chevron-right'}`}></i>
+            </button>
             <h2>Admin Panel</h2>
             <ul>
                 <li><Link to="/admin/categories">Categories</Link></li>
@@ -19,17 +26,16 @@ const Sidebar = () => {
                 <li><Link to="/admin/orders">Orders</Link></li>
 
                 {/* Dropdown */}
-                <li onClick={toggleDropdown} style={{ cursor: 'pointer' ,fontSize:'18px'}}>
+                <li onClick={toggleDropdown} style={{ cursor: 'pointer', fontSize: '18px' }}>
                     Upload
                     <span style={{ marginLeft: '5px' }}>
                         <i className={`fas ${dropdownOpen ? 'fa-chevron-up' : 'fa-chevron-down'}`}></i> {/* FontAwesome icon */}
                     </span>
                     <ul className={`dropdown ${dropdownOpen ? 'open' : ''}`}>
-                        <li ><Link to="/admin/categoryUpload">Category</Link></li>
+                        <li><Link to="/admin/categoryUpload">Category</Link></li>
                         <li><Link to="/admin/productUpload">Product</Link></li>
                     </ul>
                 </li>
-
 
                 <li><Link to="/admin/email">Email</Link></li>
             </ul>
